8efb9e0b213b1264711116f2b17086bb8b3a8fea
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2022-03-30 15:25:42 +0300

    admin only endpoints

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -6,7 +6,11 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Role } from 'src/auth/role.enum';
+import { Roles } from 'src/auth/roles.decorator';
 import { Customer } from 'src/entity/customer.entity';
 import { CustomerService } from './customer.service';
 
@@ -14,22 +18,30 @@ import { CustomerService } from './customer.service';
 export class CustomerController {
   constructor(private customerService: CustomerService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
+  @Roles(Role.Admin)
   getAllCustomers() {
     return this.customerService.getCustomers();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
+  @Roles(Role.Admin)
   addCustomer(@Body() customer: Customer) {
     return this.customerService.addCustomer(customer);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
+  @Roles(Role.Admin)
   deleteCustomer(@Param() customerId: string) {
     return this.customerService.deleteCustomer(customerId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
+  @Roles(Role.Admin)
   updateCustomer(
     @Param() customerToUpdateId: string,
     @Body() customer: Customer,
