0080cfa0d4be43c852a8fb2ea1b164d28176db70
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2022-05-10 10:23:04 +0300

    add activityType

---
+++ src/entity/activity-type.enum.ts
@@ -0,0 +1,9 @@
+export enum ActivityType {
+  MEETING = 'meeting',
+  TRAINING = 'training',
+  WORK_ON_PROJECT = 'work on project',
+  INTERNAL = 'internal',
+  VACATION = 'vacation',
+  UNPAID_LEAVE = 'unpaid leave',
+  MEDICAL_LEAVE = 'medical leave',
+}
