ebad40eac438728edd9c9051d6f68ff6da208c33
calinc2010 <33434438+calinc2010@users.noreply.github.com>
2022-06-10 15:02:48 +0300

    use process.env vars for database credentials

--- src/providers/database.provider.ts
+++ src/providers/database.provider.ts
@@ -23,17 +23,5 @@ export const databaseProvider = [
         ssl: { rejectUnauthorized: false },
         synchronize: true,
       }),
-      // await createConnection({
-      //   type: 'postgres',
-      //   host: 'ec2-52-209-185-5.eu-west-1.compute.amazonaws.com',
-      //   port: 5432,
-      //   username: 'qbvauuepktekxu',
-      //   password:
-      //     'f5d2d2108578782838d46cc0bef0b61adba2607b5ab4e7bff8ca0ea378be4d80',
-      //   database: 'd56tk5ode151l3',
-      //   entities: [Activity, User, Customer, Project],
-      //   ssl: { rejectUnauthorized: false },
-      //   synchronize: true,
-      // }),
   },
 ];
