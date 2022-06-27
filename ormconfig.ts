22c7e7766d4a29ca07076c5c048b0a5a8952d520
Coroian Calin Andrei <coro@Alexs-MBP.Dlink>
2022-06-27 15:35:19 +0300

    fix requested changes

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,11 +3,15 @@ export const connectionToDB = new DataSource({
   synchronize: false,
   name: 'default',
   type: 'postgres',
-  host: 'ec2-52-209-185-5.eu-west-1.compute.amazonaws.com',
-  port: 5432,
-  username: 'qbvauuepktekxu',
-  password: 'f5d2d2108578782838d46cc0bef0b61adba2607b5ab4e7bff8ca0ea378be4d80',
-  database: 'd56tk5ode151l3',
+  host:
+    process.env.DATABASE_HOST ||
+    'ec2-52-209-185-5.eu-west-1.compute.amazonaws.com',
+  port: (process.env.DATABASE_PORT as unknown as number) || 5432,
+  username: process.env.DATABASE_USERNAME || 'qbvauuepktekxu',
+  password:
+    process.env.DATABASE_P ||
+    'f5d2d2108578782838d46cc0bef0b61adba2607b5ab4e7bff8ca0ea378be4d80',
+  database: process.env.DATABASE_NAME || 'd56tk5ode151l3',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: ['src/migrations/*.ts'],
   migrationsTableName: 'custom_migrations_history',
