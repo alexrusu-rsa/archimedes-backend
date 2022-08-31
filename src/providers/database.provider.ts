da39017fbfafd5890540ae48a2436c9b3edd3bdf
Coroian Calin Andrei <coro@Alexs-MBP.Dlink>
2022-08-31 13:30:07 +0300

    fix final date of services on invoice compute

--- src/providers/database.provider.ts
+++ src/providers/database.provider.ts
@@ -11,11 +11,12 @@ export const databaseProvider = [
     useFactory: async () =>
       await createConnection({
         type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT as unknown as number,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_P,
-        database: process.env.DATABASE_NAME,
+        host: 'ec2-52-209-185-5.eu-west-1.compute.amazonaws.com',
+        port: 5432,
+        username: 'qbvauuepktekxu',
+        password:
+          'f5d2d2108578782838d46cc0bef0b61adba2607b5ab4e7bff8ca0ea378be4d80',
+        database: 'd56tk5ode151l3',
         entities: [Activity, User, Customer, Project, Rate],
         ssl: { rejectUnauthorized: false },
         synchronize: false,
