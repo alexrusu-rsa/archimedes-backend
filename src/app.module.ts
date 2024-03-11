1341201e35065eef891815cd5ad63b4b92166325
Calin Coroian <calincoroian@Calins-MacBook-Pro.local>
2024-03-11 11:35:10 +0200

    swagger-installed default settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,17 +10,10 @@ import { RolesGuard } from './auth/roles.guard';
 import { CustomerModule } from './customer/customer.module';
 import { ProjectModule } from './project/project.module';
 import { DateFormatService } from './date-format/date-format.service';
-import { RateController } from './rate/rate.controller';
-import { RateService } from './rate/rate.service';
 import { RateModule } from './rate/rate.module';
 import { AuthRecieveModule } from './auth-recieve/auth-recieve.module';
-import {
-  AcceptLanguageResolver,
-  I18nModule,
-  I18nService,
-  QueryResolver,
-} from 'nestjs-i18n';
-import * as path from 'path';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 @Module({
   imports: [
@@ -76,4 +69,6 @@ import * as path from 'path';
     DateFormatService,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
