70861ddf27ca90e753017d1bae7964898b4bf147
Calin Coroian <calincoroian@Calins-MBP.Dlink>
2024-07-24 13:27:46 +0300

    add proper migrations for dueDate and contractSignDate in project entity

--- src/migrations/1721741701082-dueDate.ts
+++ src/migrations/1721741701082-dueDate.ts
@@ -4,14 +4,41 @@ export class DueDate1721741701082 implements MigrationInterface {
   name = 'DueDate1721741701082';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "project" DROP COLUMN "dueDate"`);
-    await queryRunner.query(`ALTER TABLE "project" ADD "dueDate" DATE`);
+    await queryRunner.query(`
+        ALTER TABLE "project"
+        ADD COLUMN "dueDateTemp" DATE;
+    `);
+
+    await queryRunner.query(`
+        UPDATE "project"
+        SET "dueDateTemp" = to_date("dueDate", 'DD/MM/YYYY');
+    `);
+
+    await queryRunner.query(`
+        ALTER TABLE "project"
+        DROP COLUMN "dueDate";
+    `);
+
+    await queryRunner.query(`
+        ALTER TABLE "project"
+        RENAME COLUMN "dueDateTemp" TO "dueDate";
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "project" DROP COLUMN "dueDate"`);
-    await queryRunner.query(
-      `ALTER TABLE "project" ADD "dueDate" character varying`,
-    );
+    await queryRunner.query(`
+        ALTER TABLE "project"
+        ADD COLUMN "dueDate" VARCHAR;
+    `);
+
+    await queryRunner.query(`
+        UPDATE "project"
+        SET "dueDate" = to_char("dueDateTemp", 'DD/MM/YYYY');
+    `);
+
+    await queryRunner.query(`
+        ALTER TABLE "project"
+        DROP COLUMN "dueDateTemp";
+    `);
   }
 }
