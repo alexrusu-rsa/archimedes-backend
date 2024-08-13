23bcabc8b601bc623ed81707aa13cc98ff57e064
Calin Coroian <calincoroian@Calins-MBP.Dlink>
2024-08-13 14:14:38 +0300

    fix database provider

--- src/providers/database.provider.ts
+++ src/providers/database.provider.ts
@@ -13,11 +13,20 @@ export const databaseProvider = [
     useFactory: async (configService: ConfigService) =>
       await createConnection({
         type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_P'),
-        database: configService.get<string>('DATABASE_DATABASE'),
+        host:
+          configService.get<string>('DATABASE_HOST') ||
+          process.env.DATABASE_HOST,
+        port:
+          configService.get<number>('DATABASE_PORT') ||
+          (process.env.DATABASE_PORT as unknown as number),
+        username:
+          configService.get<string>('DATABASE_USERNAME') ||
+          process.env.DATABASE_USERNAME,
+        password:
+          configService.get<string>('DATABASE_P') || process.env.DATABASE_P,
+        database:
+          configService.get<string>('DATABASE_DATABASE') ||
+          process.env.DATABASE_NAME,
         entities: [Activity, User, Customer, Project, Rate],
         ssl: { rejectUnauthorized: false },
         synchronize: false,
